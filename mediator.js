fc03029081185bf046547b5218da1fe93e9edc65
Alex Scherer <alex.scherer@outlook.com>
2015-07-18 15:12:37 +0200

    Added .travis.yml, mediator code and updated package.js

--- mediator.js
+++ mediator.js
@@ -1,1 +1,19 @@
-// Write your package code here!
+var Mediator;
+
+Mediator = {
+  channels: {},
+  publish: function(name) {
+    this.channels[name].args = _.toArray(arguments);
+    this.channels[name].deps.changed();
+  },
+  subscribe: function(name) {
+    if (!this.channels[name]) {
+      this.channels[name] = {
+        deps: new Deps.Dependency,
+        args: null
+      };
+    }
+    this.channels[name].deps.depend();
+    this.channels[name].args;
+  }
+};
